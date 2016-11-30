a484456b8d528332d35a6434ba21f94b36faaf1c
Katy <ihatetoast@gmail.com>
2016-11-29 23:39:12 -0600

    storybook

--- app/scripts/Components/Subcomponents/Pets.js
+++ app/scripts/Components/Subcomponents/Pets.js
@@ -1,51 +1,18 @@
 import React from 'react';
+import { Link, hashHistory } from 'react-router';
+// import $ from 'jquery'
 
 export default React.createClass({
-	getInitialState: function () {
-		return {
-			selectedPets: "all the critters"
-		};
-	},
-	render: function(){
-		return(
-			<div>
-				<h3  className="category">I'm taking (will be egg pic)</h3>
-				<div className="eggPics">
-					<img src={'https://cinemeccanica.files.wordpress.com/2012/02/peewee-soap.jpg'}/>
+	render: function() {
+		return (
+			<section>
+				<div className="eggBtnHldr">
+					<h2>Are you plannin' or packalackin?</h2>
+					<Link to="/plannin"><button className="btnEgg majorBtnEgg" >Plannin'</button></Link>
+					<Link to="/packin"><button className="btnEgg majorBtnEgg">Packalackin'</button></Link>
 				</div>
-				<div className="radio">
-					<label>
-					<input type="radio" name='pets' className='radio' value="all the critters" checked={this.state.selectedPets === "all the critters"} onChange={this.handlePets}/>
-					all the critters
-					</label>
-				</div>
-				<div className="radio">
-					<label>
-					<input type="radio" name='pets' className='radio' value="just the hounds" checked={this.state.selectedPets === "just the hounds"} onChange={this.handlePets}/>
-					just the hounds
-					</label>
-				</div>
-				<div className="radio">
-					<label>
-					<input type="radio" name='pets' className='radio' value="just the cats" checked={this.state.selectedPets === "just the cats"} onChange={this.handlePets}/>
-					just the cats
-					</label>
-				</div>
-				<div className="radio">
-					<label>
-					<input type="radio" name='pets' className='radio' value="just myself" checked={this.state.selectedPets === "just myself"} onChange={this.handlePets}/>
-					just myself
-					</label>
-				</div>
-				<hr className="borderVee"/>
-			</div>
-			)
-	},
-	handlePets: function (changeEvent) {
-		this.setState({
-			selectedPets: changeEvent.target.value,
-		});
-		// have 2, 1, or 0 lists
+			</section>
+		);
 	}
+});
 
-})
--- app/scripts/Components/Subcomponents/Running.js
+++ app/scripts/Components/Subcomponents/Running.js
@@ -9,20 +9,20 @@ export default React.createClass({
 	render: function(){
 		return(
 			<div>
-				<h3  className="category">I hope to (replace with egg pic)</h3>
 				<div className="eggPics">
-					<img src={'https://s-media-cache-ak0.pinimg.com/originals/09/27/25/0927254cdf307b6a672955605b29bac5.jpg'}/>
+					<img src={require('./../../../assets/forrest.png')}/>
 				</div>
+				<h3  className="category">I hope to</h3>
 				<div className="radio">
 					<label>
 					<input type="radio" name='run' className='radio' value="run" checked={this.state.selectedRun === "run"} onChange={this.handleRun}/>
-					run
+					run, Katy! Run!
 					</label>
 				</div>
 				<div className="radio">
 					<label>
 					<input type="radio" name='run' className='radio' value="slack" checked={this.state.selectedRun === "slack"} onChange={this.handleRun}/>
-					slack
+					slack, Katy! Slack!
 					</label>
 				</div>
 				<hr className="borderVee"/>
